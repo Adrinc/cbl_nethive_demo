03bb5fbb2dcaf300f4e55ed9e9eebf572272a57f
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2026-01-22 11:17:36 -0800

    ruta base añadida

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: '/cbl_nethive_demo',
   integrations: [react(), tailwind()],
   output: 'static',
 });
