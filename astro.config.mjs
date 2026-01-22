ecdd938199b9180384bf7983c810ab1c3a36a2dd
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2026-01-21 17:43:24 -0800

    try to fix deplo1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 ﻿import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  output: 'static',
 });
